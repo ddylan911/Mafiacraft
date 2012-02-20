44cfba75baa7cbaefd8e92a2e06a2b3629e9a019
Ian Macali <ianmacalinao@gmail.com>
2012-02-20 02:40:39 -0600

    Added in the beginnings of /district.

---
+++ src/main/java/com/crimsonrpg/mafiacraft/cmd/DistrictCommand.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.crimsonrpg.mafiacraft.cmd;
+
+import com.crimsonrpg.mafiacraft.Mafiacraft;
+import com.crimsonrpg.mafiacraft.player.MPlayer;
+import com.crimsonrpg.mafiacraft.player.MsgColor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author simplyianm
+ */
+public class DistrictCommand {
+    public static void parseCmd(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MsgColor.ERROR + "Sorry, this command is only usable in game.");
+            return;
+        }
+
+        MPlayer player = Mafiacraft.getPlayer((Player) sender);
+
+        if (args.length < 1) {
+            doHelp(player);
+            return;
+        }
+
+        //Get the function we want to do.
+        String function = args[0];
+        List<String> largs = new ArrayList<String>(Arrays.asList(args));
+        largs.remove(0);
+
+        String result = null;
+        
+        if (largs.size() < 0) {
+            
+        }
+    }
+
+    public static String doHelp(MPlayer player) {
+        //TODO: be helpful
+        player.sendMessage(MsgColor.ERROR + "TODO: help");
+        return null;
+    }
+
+}
