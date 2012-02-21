38edc9f3c8dec4a0e93c6a1a6b6c5b2299af09d3
Ian Macali <ianmacalinao@gmail.com>
2012-02-20 18:29:48 -0600

    Added district command to the actual plugin.

--- src/main/java/com/crimsonrpg/mafiacraft/cmd/Commands.java
+++ src/main/java/com/crimsonrpg/mafiacraft/cmd/Commands.java
@@ -37,6 +37,13 @@ public class Commands {
             }
 
         });
+        plugin.getCommand("district").setExecutor(new CommandExecutor() {
+            public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+                DistrictCommand.parseCmd(cs, cmnd, string, strings);
+                return true;
+            }
+
+        });
     }
 
 }
