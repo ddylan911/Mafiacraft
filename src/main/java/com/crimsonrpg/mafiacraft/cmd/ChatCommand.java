73e6b72da6b6ed2f1da01b67d9668f41ca370c91
Ian Macali <ianmacalinao@gmail.com>
2012-02-20 22:39:45 -0600

    Cleaned up the chat command.

--- src/main/java/com/crimsonrpg/mafiacraft/cmd/ChatCommand.java
+++ src/main/java/com/crimsonrpg/mafiacraft/cmd/ChatCommand.java
@@ -8,6 +8,9 @@ import com.crimsonrpg.mafiacraft.Mafiacraft;
 import com.crimsonrpg.mafiacraft.chat.ChatType;
 import com.crimsonrpg.mafiacraft.player.MPlayer;
 import com.crimsonrpg.mafiacraft.player.MsgColor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,32 +19,43 @@ import org.bukkit.entity.Player;
  * /chat command.
  */
 public class ChatCommand {
-
     public static void parseCmd(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(MsgColor.ERROR + "You cannot use this command from console.");
+            sender.sendMessage(MsgColor.ERROR + "Sorry, this command is only usable in game.");
             return;
         }
+
         MPlayer player = Mafiacraft.getPlayer((Player) sender);
-        ChatType chatType = ChatType.valueOf(args[0]);
-        if (chatType == null) {
-            player.sendMessage(MsgColor.ERROR + "Invalid Chat.");
+
+        if (args.length < 1) {
+            doChat(player);
             return;
         }
-        if (chatType.canJoin(player)) {
-            player.setChatType(chatType);
-            player.sendMessage(MsgColor.SUCCESS + "You have joined the chat " + chatType.getName() + ".");
-        }
+
+       String  result = doChat(player, args[0]);
+       if (result != null) {
+           player.sendMessage(MsgColor.ERROR + result);
+       }
     }
 
     public static String doChat(MPlayer player) {
-        doChat(player, ChatType.DEFAULT);
+        doChat(player, "");
         return null;
     }
 
-    public static String doChat(MPlayer player, ChatType type) {
-        player.setChatType(type);
-        player.sendMessage(MsgColor.SUCCESS + "You have changed to " + type.getName(player) + " chat.");
+    public static String doChat(MPlayer player, String type) {
+        ChatType chatType = ChatType.valueOf(type);
+        if (chatType == null) {
+            return "Invalid chat type given.";
+        }
+
+        if (!chatType.canJoin(player)) {
+            return "You are not allowed to be part of that chat.";
+        }
+
+        player.setChatType(chatType);
+        player.sendMessage(MsgColor.SUCCESS + "You have changed to " + chatType.getName(player) + " chat.");
         return null;
     }
+
 }
