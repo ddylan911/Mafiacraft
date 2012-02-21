ddaeda1db08770794e50b87e089ca074a15a7f46
Ian Macali <ianmacalinao@gmail.com>
2012-02-20 18:44:22 -0600

    Set up basic section command.

--- src/main/java/com/crimsonrpg/mafiacraft/cmd/SectionCommand.java
+++ src/main/java/com/crimsonrpg/mafiacraft/cmd/SectionCommand.java
@@ -4,10 +4,45 @@
  */
 package com.crimsonrpg.mafiacraft.cmd;
 
+import com.crimsonrpg.mafiacraft.Mafiacraft;
+import com.crimsonrpg.mafiacraft.player.MPlayer;
+import com.crimsonrpg.mafiacraft.player.MsgColor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 /**
  *
  * @author simplyianm
  */
 public class SectionCommand {
-    
+    public static void parseCmd(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(MsgColor.ERROR + "Sorry, this command is only usable in game.");
+            return;
+        }
+
+        MPlayer player = Mafiacraft.getPlayer((Player) sender);
+
+        if (args.length < 1) {
+//            doHelp(player);
+            return;
+        }
+
+        //Get the function we want to do.
+        String function = args[0];
+        List<String> largs = new ArrayList<String>(Arrays.asList(args));
+        largs.remove(0);
+
+        String result = null;
+        if (largs.size() < 1) {
+        }
+        if (result != null) {
+            player.sendMessage(MsgColor.ERROR + result);
+        }
+    }
+
 }
