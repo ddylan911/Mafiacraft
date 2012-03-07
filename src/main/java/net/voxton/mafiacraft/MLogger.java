e8aa7038d46dec703484ba97b873b6974ebda298
Ian Macali <ianmacalinao@gmail.com>
2012-03-07 00:47:11 -0600

    Fixed up MLogger a ton.

--- src/main/java/net/voxton/mafiacraft/MLogger.java
+++ src/main/java/net/voxton/mafiacraft/MLogger.java
@@ -12,28 +12,61 @@ import java.util.logging.Logger;
  */
 public class MLogger {
 
+    /**
+     * The logger.
+     */
     private static final Logger LOGGER = Mafiacraft.getPlugin().getLogger();
 
+    /**
+     * The verbosity to use.
+     */
     private static final int VERBOSITY = 5;
 
+    /**
+     * Logs a message with a level of info.
+     *
+     * @param message The message to log.
+     */
     public static void log(String message) {
         log(Level.INFO, message);
     }
 
+    /**
+     * Logs a message to the console.
+     *
+     * @param level The logging level to use.
+     * @param msg The message to send.
+     */
     public static void log(Level level, String msg) {
-        msg = "[MC] " + msg;
         LOGGER.log(level, msg);
     }
 
+    /**
+     * Logs an error to the console.
+     *
+     * @param level The logging level to use.
+     * @param msg The message to send.
+     * @param thrown The Throwable to log.
+     */
     public static void log(Level level, String msg, Throwable thrown) {
-        msg = "[MC] " + msg;
         LOGGER.log(level, msg, thrown);
     }
 
+    /**
+     * Logs a message with a verbosity of 1.
+     *
+     * @param message The message to log.
+     */
     public static void logVerbose(String message) {
         logVerbose(message, 1);
     }
 
+    /**
+     * Logs a message with the specified verbosity.
+     *
+     * @param message The message to log
+     * @param level The verbosity to use.
+     */
     public static void logVerbose(String message, int level) {
         if (level > VERBOSITY) {
             return;
