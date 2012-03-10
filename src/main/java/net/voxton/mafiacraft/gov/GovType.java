28cac62db2d8662f3b4a0fe96196b5c241732d2c
Ian M <ianmacalinao@gmail.com>
2012-03-09 19:05:38 -0600

    We're AGPL.

--- src/main/java/net/voxton/mafiacraft/gov/GovType.java
+++ src/main/java/net/voxton/mafiacraft/gov/GovType.java
@@ -15,7 +15,7 @@
  * Mafiacraft is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
  * and the Voxton license along with Mafiacraft. 
